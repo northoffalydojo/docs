87afcac420a6e6da506c5741e2dc8c47330907fd
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-08 13:59:55 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,36 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
-};
+      type: 'category',
+      label: 'In person sessions',
+      collapsed: false,
+      items: ['registration-process', 'age', 'inperson-register'],
+    },
+    {
+      type: 'category',
+      label: 'Online sessions',
+      items: [
+        'what-does-my-child-learn','registration-process', 'age', 'invalid-email', 'session-duration'
+        {
+          Booking: [
+            'cant-book-ticket',
+            'minimium-tickets-2',
+            'latest-booking-link',
+          ],
+        },
+    {
+      type: 'category',
+      label: 'Volunteering',
+      items: ['volunteering-without-garda-vetting', 'garda-vetting-process', 'how-to-volunteer'],
+    },
+    {
+      type: 'category',
+      label: 'Getting in touch',
+      items: [
+        'no-autoreply',
+      ],
+    },
+  ],
+       type: 'category',
+      label: 'Emails',
+      items: ['move-to-inbox-gmail', 'subscribed-but-not-receiving-emails'],
+    },
+  
