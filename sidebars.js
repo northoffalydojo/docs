bd43c307e8241f5314c108247fd543335112be21
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-08 14:47:17 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,51 +1,12 @@
 module.exports = {
-  docs: [
-    'introduction',
-    {
-      type: 'category',
-      label: 'In-person sessions',
-      collapsed: false,
-      items: ['registration-process', 'age', 'inperson-register'],
-    },
-    {
-      type: 'category',
-      label: 'Online sessions',
-      items: [
-        'what-does-my-child-learn', 'registration-process', 'age', 'invalid-email', 'session-duration',
-        {
-          Booking: [
-            'cant-book-ticket',
-            'minimium-tickets-2',
-            'latest-booking-link',
-          ],
-        },
-        '',
-        {
-          type: 'category',
-          label: 'Volunteering',
-          items: [
-            'volunteering-without-garda-vetting',
-            'garda-vetting-process',
-            'how-to-volunteer',
-          ],
-        },
-        '',
-        {
-          type: 'category',
-          label: 'Gettting in touch',
-          items: [
-            {
-              type: 'doc',
-              id: 'no-autoreply',
-              label: 'I did not get an autoreply when I sent an email',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Emails',
-      items: ['move-to-inbox-gmail', 'subscribed-but-not-receiving-emails'],
-    },
-  ],
+  someSidebar: {
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+    In person sessions: ['registration-process, 'age', 'inperson-register'],
+    Online sessions: ['what-does-my-child-learn', 'registration-process', 'age', 'invalid-email', 'session-duration'],
+    Booking: ['cant-book-ticket', 'minimium-tickets-2', 'latest-booking-link'],
+    Volunteering: ['volunteering-without-garda-vetting', 'garda-vetting-process', 'how-to-volunteer']
+    Getting in touch: ['no-autoreply']
+    Emails: ['move-to-inbox-gmail', 'subscribed-but-not-receiving-emails'],
+  },
+};
