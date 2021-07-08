b4d021786e7330ff8dbe9eb723ef043a90d25e3f
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-08 14:39:52 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,9 @@
 module.exports = {
+  docs: [
+    'introduction',
+    {
       type: 'category',
-      label: 'In person sessions',
+      label: 'In-person sessions',
       collapsed: false,
       items: ['registration-process', 'age', 'inperson-register'],
     },
@@ -8,7 +11,7 @@ module.exports = {
       type: 'category',
       label: 'Online sessions',
       items: [
-        'what-does-my-child-learn','registration-process', 'age', 'invalid-email', 'session-duration',
+        'what-does-my-child-learn', 'registration-process', 'age', 'invalid-email', 'session-duration',
         {
           Booking: [
             'cant-book-ticket',
@@ -16,19 +19,33 @@ module.exports = {
             'latest-booking-link',
           ],
         },
-    {
-      type: 'category',
-      label: 'Volunteering',
-      items: ['volunteering-without-garda-vetting', 'garda-vetting-process', 'how-to-volunteer'],
-    },
-    {
-      type: 'category',
-      label: 'Getting in touch',
-      items: ['no-autoreply'],
-    },
+        '',
         {
-       type: 'category',
+          type: 'category',
+          label: 'Volunteering',
+          items: [
+            'volunteering-without-garda-vetting',
+            'garda-vetting-process',
+            'how-to-volunteer',
+          ],
+        },
+        '',
+        {
+          type: 'category',
+          label: 'Gettting in touch',
+          items: [
+            {
+              type: 'doc',
+              id: 'no-autoreply',
+              label: 'I did not get an autoreply when I sent an email',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
       label: 'Emails',
       items: ['move-to-inbox-gmail', 'subscribed-but-not-receiving-emails'],
     },
-  
+  ],
