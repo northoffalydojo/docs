a94c202f491d1258f0059219348c6e4c4dbcdba3
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-10 17:43:27 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,11 +2,5 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
-    In person sessions: ['registration-process, 'age', 'inperson-register'],
-    Online sessions: ['what-does-my-child-learn', 'registration-process', 'age', 'invalid-email', 'session-duration'],
-    Booking: ['cant-book-ticket', 'minimium-tickets-2', 'latest-booking-link'],
-    Volunteering: ['volunteering-without-garda-vetting', 'garda-vetting-process', 'how-to-volunteer']
-    Getting in touch: ['no-autoreply']
-    Emails: ['move-to-inbox-gmail', 'subscribed-but-not-receiving-emails'],
   },
 };
