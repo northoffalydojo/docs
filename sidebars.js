b4e276c36dd004e7e081c8b98651888eb9d18151
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-11 00:15:00 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -18,7 +18,7 @@ module.exports = {
       {
         type: 'category',
         label: 'Volunteering',
-        items: ['volunteering/volunteering-without-garda-vetting', 'volunteering/garda-vetting-process', 'volunteering/how-to-volunteer'],
+        items: ['volunteering/volunteering-without-garda-vetting', 'volunteering/garda-vetting-process', 'volunteering/volunteering-with-north-offaly-dojo'],
       },
       {
         type: 'category',
