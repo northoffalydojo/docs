017c061ba057d8f21d6c453076a96d3db8fda15e
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2021-07-10 22:43:01 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -7,13 +7,8 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'Docusaurus',
-      items: ['doc2', 'doc3'],
-    },
-    {
-      type: 'category',
       label: 'Booking',
-      items: ['booking/cant-book-ticket', 'booking/minimium-tickets-2'],
+      items: ['booking/cantbookticket', 'doc3'],
     },
   ],
 };
